734bd7c36921b3e2de5eb08c73fb5eedf2ff16e7
Mike Powers <72277454+Aphelious@users.noreply.github.com>
2022-06-27 12:33:32 -0400

    Run black code formatter'

--- app/scrappers/data_stack_jobs_scraper.py
+++ app/scrappers/data_stack_jobs_scraper.py
@@ -12,28 +12,99 @@ import pandas as pd
 import json
 
 
-html_tags = ['<p>', '<h1>', '<h2>', '<h3>', '<h4>', '<h5>', '<h6>', '<strong>', '<em>', '<abbr>', '<address>',
-             '<bdo>', '<blockquote>', '<cite>', '<q>', '<code>', '<ins>', '<del>', '<dfn>', '<kbd>', '<pre>',
-             '<samp>', '<var>', '<br>', '<div>', '<a>', '<base>', '<img>', '<area>', '<map>', '<param>', '<object>',
-             '<ul>', '<ol>', '<li>', '<dl>', '<dt>', '<dd>', '</p>', '</h1>', '</h2>', '</h3>', '</h4>', '</h5>',
-             '</h6>', '</strong>', '</em>', '</abbr>', '</address>', '</bdo>', '</blockquote>', '</cite>', '</q>',
-             '</code>', '</ins>', '</del>', '</dfn>', '</kbd>', '</pre>', '</samp>', '</var>', '</br>', '</div>',
-             '</a>', '</base>', '</img>', '</area>', '</map>', '</param>', '</object>', '</ul>', '</ol>', '</li>',
-             '</dl>', '</dt>', '</dd>', '<br />']
+html_tags = [
+    "<p>",
+    "<h1>",
+    "<h2>",
+    "<h3>",
+    "<h4>",
+    "<h5>",
+    "<h6>",
+    "<strong>",
+    "<em>",
+    "<abbr>",
+    "<address>",
+    "<bdo>",
+    "<blockquote>",
+    "<cite>",
+    "<q>",
+    "<code>",
+    "<ins>",
+    "<del>",
+    "<dfn>",
+    "<kbd>",
+    "<pre>",
+    "<samp>",
+    "<var>",
+    "<br>",
+    "<div>",
+    "<a>",
+    "<base>",
+    "<img>",
+    "<area>",
+    "<map>",
+    "<param>",
+    "<object>",
+    "<ul>",
+    "<ol>",
+    "<li>",
+    "<dl>",
+    "<dt>",
+    "<dd>",
+    "</p>",
+    "</h1>",
+    "</h2>",
+    "</h3>",
+    "</h4>",
+    "</h5>",
+    "</h6>",
+    "</strong>",
+    "</em>",
+    "</abbr>",
+    "</address>",
+    "</bdo>",
+    "</blockquote>",
+    "</cite>",
+    "</q>",
+    "</code>",
+    "</ins>",
+    "</del>",
+    "</dfn>",
+    "</kbd>",
+    "</pre>",
+    "</samp>",
+    "</var>",
+    "</br>",
+    "</div>",
+    "</a>",
+    "</base>",
+    "</img>",
+    "</area>",
+    "</map>",
+    "</param>",
+    "</object>",
+    "</ul>",
+    "</ol>",
+    "</li>",
+    "</dl>",
+    "</dt>",
+    "</dd>",
+    "<br />",
+]
 
 html_artifacts = []
 
-URL = 'https://datastackjobs.com'
+URL = "https://datastackjobs.com"
 page = requests.get(URL)
 contents = page.text
 soup = BeautifulSoup(contents, "html.parser")
-results = soup.find(name='script', id="__NEXT_DATA__")
+results = soup.find(name="script", id="__NEXT_DATA__")
 data = json.loads(results.string)
-jobs = data['props']['pageProps']['jobs']
+jobs = data["props"]["pageProps"]["jobs"]
 
 
 def clean_description_tags(description: str) -> str:
-    '''Remove html tags from description column contents'''
+    """Remove html tags from description column contents"""
     for tag in html_tags:
         if tag in description:
             description = description.replace(tag, "")
@@ -41,10 +112,8 @@ def clean_description_tags(description: str) -> str:
 
 
 df = pd.DataFrame(jobs)
-df['description'] = [clean_description_tags(x) for x in df['description']]
-df.to_csv('app/static/data/data_stack_jobs.csv', index=False)
-
-
+df["description"] = [clean_description_tags(x) for x in df["description"]]
+df.to_csv("app/static/data/data_stack_jobs.csv", index=False)
 
 
 # Generate a list of job search terms
