2a8c059d2272c74426c687b8574cc6b8e0f856e6
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-01-06 22:48:07 +0200

    Add translation data pipeline

--- app/pipeline/pipelines/apis/companies/companies_info_etl.py
+++ app/pipeline/pipelines/apis/companies/companies_info_etl.py
@@ -2,29 +2,26 @@ from typing import AsyncGenerator, Optional
 import pandas as pd
 import json
 from common.etls_common import DataPipeline, Extractor, Transformer, Loader
-from extractors.scrappers.indeed.countries_scrapper import IndeedCountriesScrapper
-from pipeline.models.country_model import CountryDim
-from pipeline.utilities.utils import Utils
-from extractors.apis.fortune_companies.fortune_companies_api_extractor import FortuneCompaniesAPIExtractor 
+from extractors.apis.companies.companies_info_api_extractor import CompaniesInfoAPIExtractor
 from pipeline.transformers.scrappers.indeed.countries_transformer import IndeedCountriesTransformer
 from utilities.decorators import timer, timer_async
 from pipeline.transformers.apis.fortune_companies_transformer import FortuneCompaniesAPITransformer
 from loguru import logger
 
-class FortuneCompaniesETL(Extractor, Transformer, Loader, DataPipeline):
+class CompaniesETL(Extractor, Transformer, Loader, DataPipeline):
     """Indeed ETL class"""
-    def __init__(self, fortune_companies_configs: dict, staging_storage_path: str, production_storage_path: str):
-        self.fortune_companies_configs = fortune_companies_configs
+    def __init__(self, company_names_df: pd.DataFrame, companies_configs: dict, access_token: str, staging_storage_path: str, production_storage_path: str):
+        self.company_names_df = company_names_df
+        self.companies_configs = companies_configs
+        self.access_token = access_token
         self.staging_storage_path = staging_storage_path
         self.production_storage_path = production_storage_path
 
     async def extract(self) -> pd.DataFrame:
         """Extract data from the Indeed Website"""
-        fortune_companies_api_extractor = FortuneCompaniesAPIExtractor(self.fortune_companies_configs)
-        fortune_companies: dict = await fortune_companies_api_extractor.extract()
-        with open(self.staging_storage_path, "w",encoding='utf-8') as file_handler:
-            file_handler.write(json.dumps(fortune_companies))
-        return fortune_companies
+        companies_api_extractor = CompaniesInfoAPIExtractor(self.company_names_df, self.companies_configs, self.access_token)
+        companies_df: pd.DataFrame = await companies_api_extractor.extract()
+        return companies_df
 
     def transform(self, fortune_companies: dict) -> pd.DataFrame:
         """Transform data"""
@@ -34,7 +31,7 @@ class FortuneCompaniesETL(Extractor, Transformer, Loader, DataPipeline):
 
     def load(self, fortune_companies_df: pd.DataFrame) -> bool:
         """Load data into the CSV"""
-        fortune_companies_df.to_csv(self.production_storage_path)
+        fortune_companies_df.to_csv(self.staging_storage_path)
         return True
     
     @timer_async
